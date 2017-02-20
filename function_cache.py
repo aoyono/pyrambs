835a00df4b78dda116447e8dedc7fd71e32cca38
oyono <adrienoyono@gmail.com>
2017-02-20 23:27:40 +0100

    Add a module docstring and other use cases with time comparisons

--- function_cache.py
+++ function_cache.py
@@ -1,3 +1,17 @@
+"""
+This file demonstrates a technique that can help accelerating function calls
+with a very simple cache.
+We use the property of mutable objects passed as default values in function
+signatures: when the mutable is modified, its value is remembered in the
+function scope, so that each time a user calls the function, the default value
+is available to the function scope.
+Note that if a user calls the function overriding the default parameter, the
+function looses its superpower :-( ! (At least, the cache is wiped and a the
+last state of the cache is retrieved for subsequent calls that don't
+override it)
+"""
+
+
 from collections import defaultdict
 from collections import Hashable
 from collections import deque
@@ -25,19 +39,24 @@ def consume(iterator, n):
 def control_cache_size(cache, size=20):
     """A function controlling the growth of the cache.
     """
-    if len(cache) > size:
-        ccache = copy.deepcopy(cache)
-        consume(starmap(ccache.pop,
-                        ((a,) for a in takewhile(lambda k: len(ccache) > size,
-                                                 cache.keys()))), None)
-        cache = copy.deepcopy(ccache)
-    print("cache keys: %s" % list(cache.keys()))
+    if cache is not None:
+        if len(cache) > size:
+            ccache = copy.deepcopy(cache)
+            consume(starmap(ccache.pop,
+                            ((a,) for a in takewhile(
+                                lambda k: len(ccache) > size, cache.keys())
+                             )), None
+                    )
+            cache = copy.deepcopy(ccache)
+        print("cache keys: %s" % list(cache.keys()))
+    else:
+        print("cache keys: %s" % cache)
 
 
 def heavy_func(key, local_cache=defaultdict(lambda: False)):
     control_cache_size(local_cache)
     computed = None
-    if isinstance(key, Hashable):
+    if isinstance(key, Hashable) and local_cache is not None:
         computed = local_cache[key]
         if computed:
             return computed
@@ -49,5 +68,26 @@ def heavy_func(key, local_cache=defaultdict(lambda: False)):
 
 
 if __name__ == '__main__':
+    # First use case: Full use of the superpower !
+    print("First use case:")
+    start = time.time()
     for i in range(50):
         heavy_func(i)
+    print("Elapsed: %s" % str(time.time() - start))
+    # Second use case: Don't use the cache alternately => a little bit faster
+    # than without a cache at all (values are computed each time the cache is
+    # ignored, but retrieved from the last state of the cache)
+    print("Second use case:")
+    start = time.time()
+    for i in range(50):
+        if i % 2 == 0:
+            heavy_func(i, local_cache=None)
+        else:
+            heavy_func(i)
+    print("Elapsed: %s" % str(time.time() - start))
+    # Third use case: Always flush the cache => We loose the superpower :-(
+    print("Third use case:")
+    start = time.time()
+    for i in range(50):
+        heavy_func(i, local_cache=None)
+    print("Elapsed: %s" % str(time.time() - start))
