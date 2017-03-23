c4e6e4a13224de367a1ae0557c914e521f898bcf
oyono <adrienoyono@gmail.com>
2017-03-23 19:42:08 +0100

    Adds a decorator for turning a function into a locally cached one
    Signed-off-by: oyono <adrienoyono@gmail.com>

--- function_cache.py
+++ function_cache.py
@@ -18,8 +18,17 @@ from collections import deque
 from itertools import starmap
 from itertools import takewhile
 from itertools import islice
+from functools import reduce
 import time
 import copy
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(name)s - [%(levelname)s]: %(message)s")
+log = logging.getLogger('function_cache')
+
+
+def set_cache_logLevel_to_debug():
+    log.setLevel(logging.DEBUG)
 
 
 def consume(iterator, n):
@@ -41,6 +50,7 @@ def control_cache_size(cache, size=20):
     """
     if cache is not None:
         if len(cache) > size:
+            log.debug("func 'control_cache_size': Maximum cache size %s reached, shrinking ...")
             ccache = copy.deepcopy(cache)
             consume(starmap(ccache.pop,
                             ((a,) for a in takewhile(
@@ -48,9 +58,9 @@ def control_cache_size(cache, size=20):
                              )), None
                     )
             cache = copy.deepcopy(ccache)
-        print("cache keys: %s" % list(cache.keys()))
+        log.debug("func 'control_cache_size': cache keys: %s" % list(cache.keys()))
     else:
-        print("cache keys: %s" % cache)
+        log.debug("func 'control_cache_size': cache keys: %s" % cache)
 
 
 def heavy_func(key, local_cache=defaultdict(lambda: False)):
@@ -65,6 +75,79 @@ def heavy_func(key, local_cache=defaultdict(lambda: False)):
     if computed is not None:
         local_cache[key] = ret_val
     return ret_val
+
+
+def list_or_tuple(obj):
+    """Test if obj is either a list or a tuple.
+    """
+    return isinstance(obj, list) or isinstance(obj, tuple)
+
+
+def hashable(obj):
+    """Test if items in obj are hashable.
+    """
+    return (list_or_tuple(obj)
+            and all(isinstance(key, Hashable) for key in obj))
+
+
+def get_unhashable_item(obj):
+    """Get one item in obj which is unhashable.
+    """
+    for it in obj:
+        if not isinstance(it, Hashable):
+            yield it
+
+
+def remove_unhashable(obj, unh):
+    """Remove unh from obj.
+    """
+    return tuple(it for it in obj if it is not unh)
+
+
+def get_hashable_subset(obj):
+    """Get a subset of obj (list or tuple) items that are hashable.
+    """
+    wh = lambda: ((hashable(
+        remove_unhashable(obj, consume(get_unhashable_item(obj), 1)))
+        or len(obj) == 0)
+        or wh())
+    return wh() and obj
+
+
+def locally_cached(decorated=None, max_cache_size=50):
+    """A decorator factory for turning a function into a locally cached one
+    with the technique used here.
+    """
+    def decorator(func):
+        def new_func(*args, local_cache=defaultdict(lambda: False), **kwargs):
+            control_cache_size(local_cache, size=max_cache_size)
+            map_fn = lambda x: ((isinstance(x, dict) and map(lambda k: x[k],
+                                                             x.keys()))
+                                or map(lambda y: y, x))
+            obj = reduce(lambda x, y: tuple(
+                [i for i in x] + [i for i in y]),
+                (it for it in map(map_fn, [args, kwargs])))
+            key = get_hashable_subset(obj)
+            if key:
+                log.debug("func '%s': Hashable key used for args '%s' "
+                          "and kwargs '%s': %s",
+                          func.__name__, args, kwargs, key)
+                res = local_cache[key]
+                if res:
+                    log.debug("func '%s': Result retrieved from local cache",
+                              func.__name__)
+                    return res
+                log.debug("The call with args '%s' and kwargs '%s' is absent "
+                          "from cache for func '%s'",
+                          args, kwargs, func.__name__)
+                local_cache[key] = func(*args, **kwargs)
+                return local_cache[key]
+            log.debug("Unable to get a hashable subset of args '%s' and kwargs"
+                      " '%s' for func '%s'",
+                      args, kwargs, func.__name__)
+            return func(*args, **kwargs)
+        return new_func
+    return (decorated and decorator(decorated)) or decorator
 
 
 if __name__ == '__main__':
